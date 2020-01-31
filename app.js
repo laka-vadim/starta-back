8f6a01515d317d78903f4aaa6246f471788e2d30
Vadim Popov <vadim@laka-web.dev>
2020-01-31 09:40:49 +0700

    all done

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const path = require('path');
 const init = require("./routes/init");
 const book = require("./routes/book");
 
 const { PORT = 3000 } = process.env;
 const app = express();
 
+mongoose.connect('mongodb://localhost:27017/bookdb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', '*');
     next();
 });
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use("/init", init);
 app.use("/book", book);
 
